85e7c7dee950dccd20afb20f28fd8bccdae9b2e7
Elias Eckle <56996771+Elias-ECKLE@users.noreply.github.com>
2023-04-25 09:43:09 +0200

    siteTermine_NonResponsive

---
+++ js/btnMecene.js
@@ -0,0 +1,9 @@
+
+var btnMecene= document.getElementById("btnMecene");
+
+console.log(btnMecene);
+
+btnMecene.addEventListener('click',()=>{
+    window.open("https://express.adobe.com/page/WPNRPwgK4doEX/");
+})
+
