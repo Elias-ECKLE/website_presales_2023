1e65e0ec1220941d00aa53abe3f1e3b54e162ec0
Elias Eckle <56996771+Elias-ECKLE@users.noreply.github.com>
2023-04-29 21:24:41 +0200

    correctionCover

--- js/coverMobile.js
+++ js/coverMobile.js
@@ -3,14 +3,19 @@ function coverAdaptable(){
     var cover = document.getElementById("cover");
     var screenWidth = window.innerWidth;
 
-    if(screenWidth<1078){
+    if(screenWidth<=600){
       //  console.log("cover changement mobile activated ");
     cover.style.backgroundImage = "url('../sources/vraiSite/cover/planchesCoverMobile.jpg')";
     cover.style.height = "100vh";
 
     }
 
-    if(screenWidth>=1078){
+    if((screenWidth<=1100) && (screenWidth>600)){
+        cover.style.backgroundImage = "url('../sources/vraiSite/cover/planchesCoverTablet.jpg')";
+        cover.style.height = "80vh";
+    }
+
+    if(screenWidth>1100){
        // console.log("cover changement normal activated ");
         cover.style.backgroundImage = "url('../sources/vraiSite/cover/planchesCover.jpg')";
         cover.style.height = "80vh";
